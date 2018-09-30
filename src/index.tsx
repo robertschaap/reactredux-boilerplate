06435c081c3bc908e01f64ab24f8e4d821e12c94
robertschaap <robert.schaap@mac.com>
2018-09-30 17:33:33 +0200

    moved serviceworker to utils

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from 'utils/registerServiceWorker';
 import Root from './Root';
 
 ReactDOM.render(
